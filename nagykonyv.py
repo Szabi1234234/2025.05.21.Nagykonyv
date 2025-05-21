4424a163260e599021db5525919b6ec186c30014
Szabi1234234 <toth.szabolcs1@diak.szbi-pg.hu>
2025-05-21 13:21:54 +0200

    konyv osztaly letrehozassa

---
+++ nagykonyv.py
@@ -0,0 +1,10 @@
+class Konyv:
+    def __init__(self, nev, szul_ev, hal_ev, cim, helyezés)
+        self.nev = nev
+        self.szul_ev = szul_ev
+        self.hal_ev = hal_ev
+        self.cim = cim
+        self.helyezés = helyezés
+
+    
+        
